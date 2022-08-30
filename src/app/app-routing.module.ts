926c6553e321cd4984fd318dc632c40adfcb13b7
Soudabeh Parsa <soudabehparsa@gmail.com>
2022-08-30 11:09:23 +0430

    cmt1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { TestComponentComponent } from './test-component/test-component.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:  '', redirectTo:  '', pathMatch:  'full' },
+  {path:"test",component:TestComponentComponent},
+  {path:"test1",component:TestComponentComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { TestComponentComponent } from './test-component/test-component.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponentComponent
   ],
   imports: [
     BrowserModule,
